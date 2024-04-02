f7eb4832a0ae87d4f17797e39bb876cf01a7766d
unknown <catiussia_tamara@hotmail.com>
2024-04-02 12:37:55 -0300

    functional component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FunctionalComponent from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <FunctionalComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const FunctionalComponent = () => {
+  const frase = "Olá, mundo! Estou aprendendo a criar componentes. :)";
+
+  return (
+    <div>
+      <p>{frase}</p>
+    </div>
+  );
+};
+
+export default FunctionalComponent;
